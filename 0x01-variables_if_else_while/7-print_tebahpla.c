ee1aeed4900d47db8b60ae591a6d855d41609738
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 03:00:36 -0700

    7-print-number

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	char a = 'z';
+	for (; a >= 'a'; a--)
+		
+		putchar(a);
+	}
+		 			
+	putchar('\n');
+	return (0);
+}
