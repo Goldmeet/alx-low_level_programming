04cd32dc95fd07426c75497f447efc41af1ad6d6
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 03:33:15 -0700

    8-print-base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	char a = 48;
+	char b = 'a';
+	while (a <= 57)
+	{
+		putchar(a);
+		a++
+	}
+	while (b <= 'f')
+	{
+		putchar(b)
+		b++
+	}
+	
+		 			
+	putchar('\n');
+	return (0);
+}
