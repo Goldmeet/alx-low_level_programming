aee84d288b10801d0a33ce352d40c3df99399a2d
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 02:15:20 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		
+		putchar(a);
+	}
+		 			
+	putchar('\n');
+	return (0);
+}
