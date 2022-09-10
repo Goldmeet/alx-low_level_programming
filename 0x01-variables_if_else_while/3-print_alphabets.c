7ad1e83f8442c59c482766035103245bbb1a2e22
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 01:49:50 -0700

    2-print-alph

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	char a;
+	char A;
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+		for (A = 'A'; A <= 'Z'; A++)
+			putchar(A); 
+			
+	putchar('\n');
+	return (0);
+}
