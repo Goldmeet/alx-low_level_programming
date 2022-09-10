d42f70e9154978caaa5394a2c70ea9de8c6725e7
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 04:25:30 -0700

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	int a = 48;
+	int b = 48;
+
+	while (a < 58)
+	{
+		
+		while (b < 58)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				Putchar(b);
+				if (!(a == 56 && b == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		       
+		   
+			b++;
+		}
+		a++;
+	}
+		 			
+	putchar('\n');
+	return (0);
+}
