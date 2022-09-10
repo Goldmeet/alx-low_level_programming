89c26c31fadb4a8ae4689c450aa4b88e80be47db
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 07:16:55 -0700

    102-print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+        int a;
+        int b;
+
+        for (a = 0; a <= 9; a++)
+        {
+
+          for (b = 0; b <= 9; b++)
+            {
+               if (a = b)
+                 {
+                   putchar('0' + a);
+                   putchar('0' + b);
+		   putchar(' ')
+		   if (a < b || a > b)
+		    {
+			putchar('0' + a);
+			putchar('0' + b);
+			
+		    }
+                   	if (a != 9 || b != 9)
+                      	{
+                        	putchar(',');
+                        	putchar(' ');
+                      	}
+                 }
+            }
+
+         }
+
+        putchar('\n');
+        return (0);
+}
