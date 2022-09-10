9e3b179f9cec0430a389627b68b189be7b94c2f4
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 02:50:36 -0700

    6-print-number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	int a = 48;
+	for (; a <= 57; a++)
+		
+		putchar(a);
+	
+		 			
+	putchar('\n');
+	return (0);
+}
