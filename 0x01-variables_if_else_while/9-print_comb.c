7d07cf54a00d969d33d0f3616308eca3a69f7846
Val <godwinvalentinomiximcom@gmail.com>
2022-09-10 03:56:09 -0700

    9-print-com

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main- entery point
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	char a = 48;
+	while (a <= 57)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(" ");
+		}
+		a++;
+	}
+		 			
+	putchar('\n');
+	return (0);
+}
