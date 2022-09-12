87556626421e465bec6624b3dd5871241e106b42
Val <godwinvalentinomiximcom@gmail.com>
2022-09-12 00:37:46 -0700

    102-unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include<unistd.h>
 /** 
-* main -prints exactly and that piece of art is useful
+* main: -prints exactly and that piece of art is useful
 * Dora Korpar, 2015-10-19, followed by a new line
-* return - the return value is 1
+*
+* Return: always  return 1
 */
 
-int main()
+int main(void)
 {
-    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return(1);
 }
