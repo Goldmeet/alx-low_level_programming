aed6b838e0f0ded7398fe031507653c98b6f5f67
Val <godwinvalentinomiximcom@gmail.com>
2022-09-12 00:16:46 -0700

    102-unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/** 
+* main -prints exactly "and that piece of art is useful"
+* "- Dora Korpar, 2015-10-19, followed by a new line
+* return - the return value is 1
+*/
+
+int main()
+{
+    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",16);
+    return(1);
+}
