17c770e553022fd1b21002a2f33697971b27041d
Val <godwinvalentinomiximcom@gmail.com>
2022-09-14 00:23:18 -0700

    last-number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,20 @@
 int print_last_digit(int n)
 {
     int i;
-    i = n % 10;
+    if(n < 0)
+    {
+        i = (n * -1) % 10;
+        _putchar(i);
+        return (i);
+        
+    }
+    else
+    {
+        i = n % 10;
+        _putchar (i);
+        return (i);
+    }
     
-    return (i); 
+    
+     
 }
