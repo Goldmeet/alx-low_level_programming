899846b0eb84951d481fb93fb9d199a9c8105e9c
Val <godwinvalentinomiximcom@gmail.com>
2022-09-14 00:02:42 -0700

    last--number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_last_digit- print the last digit
+ *@n: is of integer value
+ *Return: Always return i
+ */
+int print_last_digit(int n)
+{
+    int i;
+    i = n % 10;
+    
+    return (i); 
+}
