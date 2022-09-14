816fbfa30b8750e1d5c186a927f4d476aaee8818
Val <godwinvalentinomiximcom@gmail.com>
2022-09-14 10:31:10 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - print addition of two values
+ *
+ *Return: Always result.
+ */
+int add(int a, int b)
+{
+        int result;
+        result = a + b;
+        return result;
+}
