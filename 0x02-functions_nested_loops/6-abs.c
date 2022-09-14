e8a3e317c48a68663c186b7f1acd123d882771aa
Val <godwinvalentinomiximcom@gmail.com>
2022-09-13 23:46:23 -0700

    -abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs- print positive of any value
+ *@n:is of integer type
+ *
+ *Return: Always return n
+ */
+int _abs(int n)
+{
+    
+    if (n < 0)
+    {
+         n *= -1;
+        return (n);
+    }
+    else 
+    
+        return (n); 
+}
