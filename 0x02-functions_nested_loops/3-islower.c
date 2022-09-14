a1c8b3794d6fba60caa8c1967c5abbf5520700be
Val <godwinvalentinomiximcom@gmail.com>
2022-09-13 19:03:52 -0700

    is-lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_islower- print character form
+ *
+ *Return: Always renturn 1 or 0
+ */
+int _islower(int c)
+{
+    if (c >= 97 & c  <= 122)
+        return (1);
+    else
+        return (0);         
+    
+}
