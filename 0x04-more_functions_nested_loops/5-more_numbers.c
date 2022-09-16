8da2daa7ef3d828780bf336cb30557869d06d14f
Val <godwinvalentinomiximcom@gmail.com>
2022-09-16 02:26:30 -0700

    5-more-numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+/**
+ *more_numbers- print numbers
+ *
+ *Return: return void.
+ */
+void more_numbers(void)
+{
+
+  int i = 0;
+  while (i <= 10)
+    {
+      int j;
+      for (; j <= 14; j++)
+	_putchar('0' + j);
+      _putchar('\n');
+
+    }
+  _putchar('\n')
+}
