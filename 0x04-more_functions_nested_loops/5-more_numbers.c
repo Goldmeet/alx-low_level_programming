894f036f1463dfbd74e99f14dc579d3fb6356574
Val <godwinvalentinomiximcom@gmail.com>
2022-09-16 02:50:34 -0700

    5-more-numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,18 @@
 void more_numbers(void)
 {
 
-  int i = 0;
-  while (i < 10)
+  int i = 1;
+  while (i <= 10)
     {
       int j = 0;
-      for (; j <= 14; j++)
-	_putchar('0' + j);
+      while(j <= 14)
+	{
+	  _putchar('0' + j);
+	  j++;
+	}
+      
       _putchar('\n');
+      i++;
 
     }
   _putchar('\n');
