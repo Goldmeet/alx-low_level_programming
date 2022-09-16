0f0173b4e69ae2a35d94374cedd091a3c64fb347
Val <godwinvalentinomiximcom@gmail.com>
2022-09-16 01:11:24 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul- check the multiplies two digits
+ *@a: s an integer.
+ *@b: is an integer
+ *Return: Always return a value.
+ */
+int mul(int a, int b)
+{
+  int result;
+  result = a * b;
+  return (result);
+
+}
