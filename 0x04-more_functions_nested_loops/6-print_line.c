f93b0371e89a962ebe7f930ff3e11a8737f76181
Val <godwinvalentinomiximcom@gmail.com>
2022-09-16 05:14:30 -0700

    print-line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_line- print line
+ *@n: is an integer.
+ *Return: return void.
+ */
+void print_line(int n)
+{
+  if (n <= 0)
+    {
+      _putchar('\n');
+
+    }
+  if (n > 0)
+    {
+      while (n > 0)
+        {
+          _putchar('_');
+          n--;
+
+        }
+      _putchar('\n');
+
+}
