e9e57068d244280e6549d8a6a58cfad3a04f0ca5
Val <godwinvalentinomiximcom@gmail.com>
2022-09-16 01:52:12 -0700

    number count

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers- print numbers from 0 to 9.
+ *
+ *Return: return void.
+ */
+void print_numbers(void)
+{
+  int i;
+  for (i = 0; i <= 9; i++)
+    _putchar('0' + i);
+  _putchar('\n')
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
