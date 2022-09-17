6633b5c3cf25373da71980086d25fb31107da5d6
Val <godwinvalentinomiximcom@gmail.com>
2022-09-16 20:19:17 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,28 +6,21 @@
  */
 void print_square(int size)
 {
-   
-  if (size<= 0)
+    if (size > 0)
     {
-      _putchar('\n');
-
-    }
-    
-  if (size > 0)
-    { 
         int i = 0;
-      while (i < size)
+        while (i < size)
         {
-              int x = 0;
-              while (x < size)
-              {
-                  _putchar('\#');
-                  x++;
-              }
-              
-              _putchar('\n');
-              i++;
-	}
-      _putchar('\n');
+            int x = 0;
+            while (x < size)
+            {
+                _putchar('\#');
+                x++;
+            }
+            _putchar('\n');
+            i++;
+        }
     }
+    else (size<= 0)
+             _putchar('\n');
 }
