edfc2829cbd25fb13ef94228d8a10ac4bdf53ab8
Val <godwinvalentinomiximcom@gmail.com>
2022-09-21 15:03:15 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strcat- concatinate two string.
+ *@dest: pointer of type character.
+ *@src: pointer of type character 
+ *Return: Alway return a pointer.
+ */
+char *_strcat(char *dest, char *src)
+{
+                 int i;
+                 int len = 0;
+
+                 for(i = 0; *(dest + i) != '\0'; i++)
+                             len++;
+                 int dest_len  = len;
+                 for (i = 0; *(src + i) != '\0'; i++)
+                             dest[dest_len + i] = *(src + i);
+		 dest[dest_len + i] =  '\0';
+
+                 return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+#Advance Pointer Project
+0 -function that concatenates two strings
+1 -function that concatenates two strings.
+2 -function that copies a string.
+4 -function that compares two strings.
+5 -function that changes all lowercase letters of a string to uppercase
+6 -function that capitalizes all words of a string.
+7 -function that encodes a string into 1337.
