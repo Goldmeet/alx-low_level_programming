eda8f0f9ccea53546a93f282614c0c9ded383236
Val <godwinvalentinomiximcom@gmail.com>
2022-09-22 15:36:23 -0700

    encoding

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,11 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *c);
 char *cap_string(char *);
+char *leet(char *c);
+
+
+
+
+
 
 #endif /* MAIN_H */
