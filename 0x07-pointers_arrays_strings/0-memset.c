6790044bc98bb278acc6ffb2245d725d64873748
Val <godwinvalentinomiximcom@gmail.com>
2022-09-26 21:19:09 -0700

    memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- **_menset- print the memory contant
+ *_menset- print the memory contant
  *@s: is a pointer
  *@b: is a constant character
  *@n: is an unsigned integer
@@ -9,7 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
     unsigned i;
-    for (i = 0; i <= n; i++)
+    for (i = 0; i < n; i++)
         s[i] = b;
     return (s);
 
