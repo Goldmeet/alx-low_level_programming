56aef0a89d38aa6759fd2cba29cfef4fac0b4829
Val <godwinvalentinomiximcom@gmail.com>
2022-09-27 10:42:41 -0700

    search

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,11 @@ char *_strchr(char *s, char c)
 
     while (*s != '\0')
     {
-        if (*s && *s == c)
+        if (*s == c)
             return (s);
-        else
+        if (*s != c && *s == '\0')
             return (NULL);
+	*s++;
     }
 
 
