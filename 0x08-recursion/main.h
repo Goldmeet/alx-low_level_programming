c8a243691426a4d7957c2d2d43669b2c3b4b659c
Val <godwinvalentinomiximcom@gmail.com>
2022-09-29 20:34:41 -0700

    prime numbet

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,12 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_sqrt(int n, int square);
+int is_prime_number(int n);
+int is_prime(int n, int i);
+
+
+
+
 
 
 #endif /* MAIN_H */
