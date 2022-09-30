62045f58f1a14615153e7d0480b9b1ef54f5a7f4
Val <godwinvalentinomiximcom@gmail.com>
2022-09-29 21:08:39 -0700

    drome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,7 +14,7 @@ int is_sqrt(int n, int square);
 int is_prime_number(int n);
 int is_prime(int n, int i);
 int palindrome_check(char *s, int len, int i);
-int is_palindrome(char *s)
+int is_palindrome(char *s);
 
 
 
