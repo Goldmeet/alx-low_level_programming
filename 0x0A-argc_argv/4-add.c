9bfe3872b6d6fceac40cbc8b1a0ea2bb67713d51
Val <godwinvalentinomiximcom@gmail.com>
2022-10-01 01:47:20 -0700

    infite addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,35 @@
-#include <stdio.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- *main- print sum of numbers.
- *@argc: argument count
- *@argv: argument vector.
- *Return: return 0
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
  */
-
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-    int i;
-    int sum = 0;
+	int i, j, sum;
 
-    if (argc == 1)
-        printf("0\n");
-
-    for (i = 1; i < argc; i++)
-    {
-        if(!((48 >= argv[i] <= 57)|| argv[i] == '-'))
-        {
-
-            printf("Error\n");
-            return (1);
-        }
-        sum += atoi(argv[i]);
-
-    }
-    printf("sum '=' %d\n", sum);
-    return (0);
-
-
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
