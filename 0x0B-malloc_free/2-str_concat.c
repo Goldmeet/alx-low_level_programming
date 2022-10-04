35aa1d56fff6db221887bde76cf15ff56293fd78
Val <godwinvalentinomiximcom@gmail.com>
2022-10-04 13:27:01 -0700

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-
+ lines (41 sloc)  724 Bytes
 
 #include "main.h"
 #include <stdlib.h>
@@ -15,12 +15,6 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j, size = 0;
 	char *str;
 
-	return (NULL);
-        if (s1 == NULL && s2 == NULL)
-        {
-                str[0] = '\0';
-                return (NULL);
-        }
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; i++)
 			size++;
@@ -30,11 +24,16 @@ char *str_concat(char *s1, char *s2)
 	str = malloc(sizeof(char) * (size + 1));
 	if (str == NULL)
 		return (NULL);
-       
+	if (s1 == NULL && s2 == NULL)
+	{
+		str[0] = '\0';
+		return (str);
+	}
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; i++)
 			str[i] = s1[i];
-      
+	if (s1 == NULL)
+		i = 0;
 	if (s2 != NULL)
 		for (j = 0; s2[j] != '\0'; j++)
 		{
