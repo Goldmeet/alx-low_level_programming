c998b509de0b6d5111509d887ad51b8b9f1d92d1
Val <godwinvalentinomiximcom@gmail.com>
2022-10-04 21:31:03 -0700

    string to word

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,6 +10,8 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+int wordnos(char *s);
+char **strtow(char *str);
 
 
 #endif
