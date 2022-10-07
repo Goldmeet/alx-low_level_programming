e4197ff56d30d3f23a88b7c6c3a4e7e4d5e0e1ab
Val <godwinvalentinomiximcom@gmail.com>
2022-10-07 15:34:19 -0700

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,10 +20,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; i++)
-                len1++;
+		  len1++;
 	if (s2 != NULL)
 		for (i = 0; s2[i] != '\0'; i++)
-                len2++;
+		  len2++;
 	if (n >= len2)
             n = len2;
 	str = malloc(sizeof(char*) * (len1 + n + 1));
