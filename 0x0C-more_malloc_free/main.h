fb9db8bd81abc9ed895a1c15ad3c51fabfbd720d
Val <godwinvalentinomiximcom@gmail.com>
2022-10-07 20:14:25 -0700

    multiplication in base 10

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _isNum(char *num);
+
+char *_memset(char *s, char b, unsigned int n);
+
+int _strlen(char *s);;
+
 #endif
