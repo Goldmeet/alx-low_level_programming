ff37ca2a629fe1a5ab674f7694d084c4800dc828
Val <godwinvalentinomiximcom@gmail.com>
2022-10-07 19:45:34 -0700

    arrays of numbers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,26 +1,26 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * array_range - prints arrays of number
- * @min: the minimum integer
- * @max: the maximum integer
+ * *array_range - creates an array of integers and stores numbers min to max,
+ * both inclusive in the array
+ * @min: smallest number to be stored
+ * @max: largest number to be stored
  *
- * Return: return an integer pointer.
+ * Return: pointer to the newly created array on success,
+ * else NULL if min > max or if malloc fails
  */
 int *array_range(int min, int max)
 {
-        int i; 
-        unsigned int range;
-        int *arr;
-        if ( min > max)
-                return (0);
-        range = max - min
-        arr = malloc(sizeof(arr) * (range + 1));
-        if (arr == 0)
-                return (0);
-        for (i = 0; (min + i) <= max; i++)
-                arr[i] = min;
-        return (arr);
+	int *arr;
+	int len, num, index;
 
+	if (min > max)
+		return (0);
+	len = (max - min) + 1;
+	arr = malloc(sizeof(int) * len);
+	if (arr == 0)
+		return (0);
+	for (num = min, index = 0; num <= max; index++, num++)
+		*(arr + index) = num;
+	return (arr);
 }
