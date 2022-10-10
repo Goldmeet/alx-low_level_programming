36d101ef0f816db64fe86c8210cab03be4409951
Val <godwinvalentinomiximcom@gmail.com>
2022-10-10 13:39:42 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- print the file name
+ *@argc: counter
+ *@argv: vecter of arrays of strings
+ *Return: return an integer
+ */
+int main(int argc,char *argv[])
+{
+  (void)argc;
+  printf("%s\n", argv[0]);
+  return (0);
+
+}
