72fd03bb026d6cf74c2b5b98fd20827a8a64f7dc
Val <godwinvalentinomiximcom@gmail.com>
2022-10-10 13:52:50 -0700

    print the name of the c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include<stdio.h>
 /**
  *main- print the file name
- *@argc: counter
- *@argv: vecter of arrays of strings
+ *
  *Return: return an integer
  */
-int main(int argc,char *argv[])
+int  main(void)
 {
-  (void)argc;
-  printf("%s\n", argv[0]);
-  return (0);
+printf("%s\n", __FILE__);
+return (0);
 
 }
