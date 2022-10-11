8458eba9d428e5b70043700a4ee5de045493ade7
Val <godwinvalentinomiximcom@gmail.com>
2022-10-11 15:14:55 -0700

    struct structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- contain the detials of a pet
+ *@name: string pointer
+ *@age: float value
+ *@owner: the name.of the owner
+ *
+ */
+
+
+
+struct dog{
+char *name;
+float age;
+char *owner;
+
+
+};
+
+#ebdif /* DOG_H */
