6fc602287dcbef23127b052c1e803ec303104e56
Val <godwinvalentinomiximcom@gmail.com>
2022-10-11 16:27:06 -0700

    initialization of struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+/**
+ *init_dog - print dog details
+ *@d: pointer
+ *@name: string pointer
+ *@age: float
+ *@owner: string pointer
+ *
+ *Description: The function print details of a dog
+ *
+ *Return: return void.
+ */
+
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d).name = name;
+(*d).age =age;
+(*d).owner = owner;
+
+
+}
