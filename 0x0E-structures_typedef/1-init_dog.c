acdb2b4751e59548d34e959581ca8d93967c90f1
Val <godwinvalentinomiximcom@gmail.com>
2022-10-11 16:35:04 -0700

    initialization

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 /**
- *init_dog - print dog details
+ *init_dog- print dog details
  *@d: pointer
  *@name: string pointer
  *@age: float
@@ -16,6 +16,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+  if (d == NULL)
+    return (1);
+
 (*d).name = name;
 (*d).age =age;
 (*d).owner = owner;
