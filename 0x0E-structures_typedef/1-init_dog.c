61cb8b78b0a9d8ea3c3cf337be610246afd6deb9
Val <godwinvalentinomiximcom@gmail.com>
2022-10-11 16:40:59 -0700

    initialization

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,11 +16,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  if (d == NULL)
+  if (d == 0)
     return (1);
 
 (*d).name = name;
-(*d).age =age;
+(*d).age = age;
 (*d).owner = owner;
 
 
