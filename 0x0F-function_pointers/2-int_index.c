0c4da893f0489e3fc4deb4e718442e971c3abe12
Val <godwinvalentinomiximcom@gmail.com>
2022-10-13 22:38:57 -0700

    search

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,26 +1,22 @@
+
+
 #include "function_pointers.h"
 
 /**
- *in_index - check if a number is equal to 98
- * @array: array of integer
- *@size: size of the array
- *@cmp:funtion pointer
+ * int_index - searches for an integer
  *
- * Return: return an integer.
+ * @array: pointer to array
+ * @size: size of array
+ * @cmp: function to compare and return match if it exists
+ * Return: index of first match or -1 if not match or fail
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int i;
-  if (size <= 0)
-    return (-1);
-  if (cmp && (array && size > 0))
-    {
-      for (i = 0; i < size; i++)
-	{
-	  cmp(array[i]);
-	  return (i);
-	}
-    }
-  return (-1);
+	int i;
 
+	if (array && cmp && size > 0)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	return (-1);
 }
