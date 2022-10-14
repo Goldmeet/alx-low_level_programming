1514ef7bc4e999d240be6ab01c83878fdc98939a
Val <godwinvalentinomiximcom@gmail.com>
2022-10-13 19:02:10 -0700

    printer eleents of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
+
 /**
  * array_iterator - prints arrays of numbers.
  * @array: array of integers
- *@size_t: size of the array.
+ *@size: size of the array.
  *@action: is a function pointer.
  *
  * Return: Nothing.
@@ -12,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned  int i;
   if (action && size > 0)
     for (i = 0; i < size; i++)
-      action(arrray[i]);
+      action(array[i]);
 
 }
