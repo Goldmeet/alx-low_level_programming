58a72ec0fa70d0bfe3effe4acc57bddc318c7cbf
Val <godwinvalentinomiximcom@gmail.com>
2022-10-13 17:56:07 -0700

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+
+
+
+#endif /* FUNCTION_POINTERS_H */
