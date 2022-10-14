94d42515a4444becd9e236488e461c7c7a7fd29b
Val <godwinvalentinomiximcom@gmail.com>
2022-10-13 18:57:07 -0700

    printer eleents of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  int i;
+unsigned  int i;
   if (action && size > 0)
     for (i = 0; i < size; i++)
       action(arrray[i]);
