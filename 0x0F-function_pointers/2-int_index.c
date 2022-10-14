ddac488e979c9070e4358f235bd3e6ebb609b475
Val <godwinvalentinomiximcom@gmail.com>
2022-10-13 22:52:01 -0700

    search

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
+	if (size <= 0)
+	  return (-1);
 	if (array && cmp && size > 0)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
