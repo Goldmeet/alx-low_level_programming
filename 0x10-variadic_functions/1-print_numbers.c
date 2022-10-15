8541521ac5ceb9e5e4425da67cb9ce83808fb8ec
Val <godwinvalentinomiximcom@gmail.com>
2022-10-15 10:11:17 -0700

    printthe parameter

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,13 +12,14 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
   unsigned int i;
-  
+  int num;
   va_list my_num;
 
   va_start(my_num, n);
   for (i = 0; i < n; i++)
     {
-      printf("%d", va_arg(my_num, int));
+      num = va_arg(my_num,int);
+      printf("%d", num, int));
       if (separator)
 	printf("%s", separator);
     }
