e6046de518bf13616b2a984ddf14df1eb87823f1
Val <godwinvalentinomiximcom@gmail.com>
2022-10-21 04:12:35 -0700

    first list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,11 @@
 
 #include "lists.h"
 #include <stdlib>
+
 /**
  *print_list - singly link list.
  * @h: a pointer to a struct.
+ *
  * Description: singly linked list node structure
  * Return: return the count.
  */
@@ -15,12 +17,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		count++;
+
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 
 		printf("[%d]%s\n", h->len, h->str);
 		h = h->next;
 	}
-	return (count)
+	return (count);
 }
 
