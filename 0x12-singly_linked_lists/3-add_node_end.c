ae28450512c28ff53fc237c44976cc90d712733e
Val <godwinvalentinomiximcom@gmail.com>
2022-10-22 00:20:29 -0700

    end node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,29 +9,22 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *new_nod, *ptr;
-        size_t n;
+	list_t *new_nod, *ptr;
+	size_t n;
+
 	ptr = *head;
-
-        new_node = malloc(sizeof(list_t));
-        if (new_node == NULL)
-                return (NULL);
-
-        new_node->str = strdup(str);
-
-        for (n = 0; str[n]; n++)
-                ;
-
-        new_node->len = n;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	for (n = 0; str[n]; n++)
+		;
+	new_node->len = n;
 	new_node->next = NULL;
-	
-
 	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
 	}
 	ptr->next = new_node;
-
-
-        return (ptr);
+	return (ptr);
 }
