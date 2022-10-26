4a8893dfd1d39fad9edc25595a8e1991bbc505c7
Val <godwinvalentinomiximcom@gmail.com>
2022-10-26 05:04:56 -0700

    free head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,14 @@
  * Description: singly linked list node structure
  *Return: return unsigned value
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-        listint_t *temp = head;
+        listint_t *temp = *head;
         while (temp != NULL)
         {
                 temp = temp->next;
-                free(head);
+                free(*head);
                 head = temp;
         }
-	head = NULL;
+	*head = NULL;
 }
