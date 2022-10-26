1f6f311c2d61d7a8fd0b9df78798fdea69fb6b20
Val <godwinvalentinomiximcom@gmail.com>
2022-10-26 05:23:38 -0700

    list free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,7 +13,7 @@ void free_listint2(listint_t **head)
         {
                 temp = temp->next;
                 free(*head);
-                head = temp;
+                *head = temp;
         }
-	*head = NULL;
+	head = NULL;
 }
