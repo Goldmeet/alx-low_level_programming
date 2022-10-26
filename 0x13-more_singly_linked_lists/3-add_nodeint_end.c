32ad325733ccc73ed05bf71e7cc47a78618b3a4a
Val <godwinvalentinomiximcom@gmail.com>
2022-10-26 04:45:39 -0700

    list operation

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 /**
  * listint_t *add_nodeint_end - singly linked list
  * @n: integer
@@ -9,20 +10,26 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *ptr;
-	ptr = head;
+	
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
-	while (ptr->next != NULL)
-	{
-		ptr = ptr->next;
-	}
+	ptr = *head;
+
+	while(ptr->next != NULL)
+		{
+		   ptr = ptr->next;
+		}
 	ptr->next = new_node;
-	return (ptr);
+	return (new_node);
 }
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - singly linked list
+ * @head: pointer to struct
+ *
+ * Description: singly linked list node structure
+ *Return: return unsigned value
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
