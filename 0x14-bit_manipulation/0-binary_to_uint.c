1f2c41b0369e78457178a044a5ffd2ff0dc25631
Val <godwinvalentinomiximcom@gmail.com>
2022-10-29 12:07:14 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec_num;
+	int i, len, rem, base;
+
+	base = 1;
+	dec_num = 0;
+	len = strlen(b);
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] > 47 && b[i] < 50)
+		{
+			rem = b[i] - 48;
+			dec_num = dec_num + rem * base;
+			base = 2 * base;
+		}
+		else
+			return (NULL);
+
+	}
+	return (dec_num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int _putchar(int c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif /* MAIN_H */
