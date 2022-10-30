f530ddf422c855366315f344e6e105d6dad4d74f
Val <godwinvalentinomiximcom@gmail.com>
2022-10-30 09:00:49 -0700

    binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <conio.h>
 
-int _putchar(int c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(int c);
+
 
 #endif /* MAIN_H */
